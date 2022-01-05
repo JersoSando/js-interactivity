a0244c674ca870e6f075550d8e24bc5e3f12e00d
Jerso Sando <jerso.sando@gmail.com>
2022-01-05 15:17:51 -0700

    intermediate commit

--- index.js
+++ index.js
@@ -27,14 +27,26 @@ function addMovie(event){
 function deleteMovie(event) {
     event.target.parentNode.remove();
     message.textContent = 'Movie Deleted!'
+    message.textContent = `${event.target.parentNode.firstChild.textContent}`
+    revealMessage()
 }
 
 function crossOffMovie(event) {
     event.target.classList.toggle("checked")
 
     if (event.target.classList.contains('checked') === true) {
-        message.textContent = 'Movie watched!'
+        // message.textContent = 'Movie watched!'
+        message.textContent = `${event.target.textContent} watched!`
     } else {
-        message.textContent = 'Movie added back!'
+        // message.textContent = 'Movie added back!'
+        message.textContent = `${event.target.textContent} added back!`
     };
+    revealMessage()
 };
+
+function revealMessage() {
+    message.classList.remove('hide');
+    setTimeout(()=> {
+        message.classList.add('hide')
+    }, 1000)
+}
